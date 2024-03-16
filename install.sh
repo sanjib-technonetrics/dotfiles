01040afdce8e3c35b7c75b3d36a865a3d34a67bf
sanjib-technometrics <sanjib@technometrics.net>
2024-03-16 19:59:26 +0600

    adding docker

--- install.sh
+++ install.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-
-sudo locale-gen
-sudo update-locale
-
-sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install -y zsh fzf nano
-
-sudo chsh -s $(which zsh) coder
-
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 cp .zshrc ~/.zshrc
 cp .p10k.zsh ~/.p10k.zsh
@@ -16,10 +8,3 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
 git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git \
   ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
-zsh -c 'source .zshrc'
-
-# install and start code-server
-curl -fsSL https://code-server.dev/install.sh | sh -s -- --method=standalone --prefix=/tmp/code-server --version 4.11.0
-/tmp/code-server/bin/code-server --auth none --port 13337 >/tmp/code-server.log 2>&1 &
-
-nohup sudo dockerd >/dev/null 2>&1 &
