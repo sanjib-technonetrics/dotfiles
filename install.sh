1f46b5bb94067a9f3e8a96adc199d3d08765c884
sanjib-technometrics <sanjib@technometrics.net>
2024-03-16 17:26:41 +0600

    adding docker

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
-#!/bin/zsh
+#!/bin/bash
 
-sudo apt-get update && sudo apt-get upgrade
-
-# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-# cp .zshrc ~/.zshrc
-# cp .p10k.zsh ~/.p10k.zsh
+sudo apt-get update && sudo apt-get upgrade && sudo apt-get install zsh fzf nano
+sudo chsh -s $(which zsh) coder
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+cp .zshrc ~/.zshrc
+cp .p10k.zsh ~/.p10k.zsh
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
 git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git \
   ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
-source .zshrc
+zsh -c 'source .zshrc'
 
 # install and start code-server
 curl -fsSL https://code-server.dev/install.sh | sh -s -- --method=standalone --prefix=/tmp/code-server --version 4.11.0
