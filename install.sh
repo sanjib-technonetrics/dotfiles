89533228e0b03740480b4c399c8cf32624a5d560
sanjib-technometrics <sanjib@technometrics.net>
2024-03-16 15:31:25 +0600

    adding docker

--- install.sh
+++ install.sh
@@ -1,17 +1,21 @@
 #!/bin/zsh
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-cp .zshrc ~/.zshrc
-cp .p10k.zsh ~/.p10k.zsh
+sudo apt-get update && sudo apt-get upgrade
+
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# cp .zshrc ~/.zshrc
+# cp .p10k.zsh ~/.p10k.zsh
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
 git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git \
   ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
 source .zshrc
-set -e
 
-# install and start code-server
 # install and start code-server
 curl -fsSL https://code-server.dev/install.sh | sh -s -- --method=standalone --prefix=/tmp/code-server --version 4.11.0
 /tmp/code-server/bin/code-server --auth none --port 13337 >/tmp/code-server.log 2>&1 &
+
+# install docker
+# curl -fsSL https://get.docker.com -o get-docker.sh
+# sh get-docker.sh
